59220f39b1125d1d0390d096b34f2809e6e24c83
zilav <zilav18@gmail.com>
2015-11-18 23:21:40 -0800

    New Fallout4 Record STAG

--- wbDefinitionsFO4.pas
+++ wbDefinitionsFO4.pas
@@ -13614,8 +13614,12 @@ begin
     wbEDID
   ]);
 
-  wbRecord(STAG, 'STAG', [
-    wbEDID
+  wbRecord(STAG, 'Attack Sound', [
+    wbEDID,
+    wbRArray('Sounds', wbStruct(TNAM, 'Sound', [
+      wbFormIDCk('Sound', [SNDR]),
+      wbString('Action')
+    ]))
   ]);
 
   wbRecord(TERM, 'TERM', [
