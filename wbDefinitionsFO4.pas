c22397daf7d075cf49d8eeb768b0b2bb2ebf3d8a
zilav <zilav18@gmail.com>
2015-11-19 16:23:53 -0800

    New Fallout4 Record MSWP

--- wbDefinitionsFO4.pas
+++ wbDefinitionsFO4.pas
@@ -13672,8 +13672,17 @@ begin
     wbEDID
   ]);
 
-  wbRecord(MSWP, 'MSWP', [
-    wbEDID
+  wbRecord(MSWP, 'Material Swap', [
+    wbEDID,
+    wbString(FNAM),
+    wbRArray('Unknown',
+      wbRStruct('Unknown', [
+        wbString(BNAM),
+        wbString(SNAM),
+        wbString(FNAM),
+        wbUnknown(CNAM)
+      ], [])
+    )
   ]);
 
   wbRecord(NOCM, 'NOCM', [
