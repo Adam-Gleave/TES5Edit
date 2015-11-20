5811f0d1b7f2d53293b2f6c70f762e2f695726ac
zilav <zilav18@gmail.com>
2015-11-19 23:24:48 -0800

    Updated EQUP

--- wbDefinitionsFO4.pas
+++ wbDefinitionsFO4.pas
@@ -9938,7 +9938,8 @@ begin
   wbRecord(EQUP, 'Equip Type', [
     wbEDID,
     wbArray(PNAM, 'Slot Parents', wbFormID('Can Be Equipped'), 0, nil, nil, cpNormal, False),
-    wbInteger(DATA, 'Use All Parents', itU32, wbEnum(['False', 'True']))
+    wbInteger(DATA, 'Use All Parents', itU32, wbEnum(['False', 'True'])),
+    wbUnknown(ANAM)
   ]);
 
   wbRecord(RELA, 'Relationship',
